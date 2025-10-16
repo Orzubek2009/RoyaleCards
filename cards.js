61394f8ac612bd870ac99dcfc775c7e1ffe0ae54
Orzubek2009 <orzubek200919@gmail.com>
2025-10-16 16:53:56 -0500

    Add files via upload

---
+++ cards.js
@@ -0,0 +1,728 @@
+const cards = [
+    {
+        "name": "Skeletons",
+        "cost": "1",
+        "icon": "/clash-royale/res/img/cards/skeletons.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Ice Spirit",
+        "cost": "1",
+        "icon": "/clash-royale/res/img/cards/ice_spirit.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Fire Spirit",
+        "cost": "1",
+        "icon": "/clash-royale/res/img/cards/fire_spirit.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Electro Spirit",
+        "cost": "1",
+        "icon": "/clash-royale/res/img/cards/electro_spirit.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Heal Spirit",
+        "cost": "1",
+        "icon": "/clash-royale/res/img/cards/heal_spirit.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Goblins",
+        "cost": "2",
+        "icon": "/clash-royale/res/img/cards/goblins.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Bomber",
+        "cost": "2",
+        "icon": "/clash-royale/res/img/cards/bomber.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Spear Goblins",
+        "cost": "2",
+        "icon": "/clash-royale/res/img/cards/spear_goblins.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Ice Golem",
+        "cost": "2",
+        "icon": "/clash-royale/res/img/cards/ice_golem.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Bats",
+        "cost": "2",
+        "icon": "/clash-royale/res/img/cards/bats.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Wall Breakers",
+        "cost": "2",
+        "icon": "/clash-royale/res/img/cards/wall_breakers.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Suspicious Bush",
+        "cost": "2",
+        "icon": "/clash-royale/res/img/cards/suspicious_bush.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Berserker",
+        "cost": "2",
+        "icon": "/clash-royale/res/img/cards/berserker.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Knight",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/knight.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Archers",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/archers.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Minions",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/minions.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Skeleton Army",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/skeleton_army.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Ice Wizard",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/ice_wizard.webp",
+        "rarity": "legendary"
+    },
+    {
+        "name": "Guards",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/guards.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Princess",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/princess.webp",
+        "rarity": "legendary"
+    },
+    {
+        "name": "Miner",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/miner.webp",
+        "rarity": "legendary"
+    },
+    {
+        "name": "Mega Minion",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/mega_minion.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Dart Goblin",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/dart_goblin.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Goblin Gang",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/goblin_gang.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Bandit",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/bandit.webp",
+        "rarity": "legendary"
+    },
+    {
+        "name": "Royal Ghost",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/royal_ghost.webp",
+        "rarity": "legendary"
+    },
+    {
+        "name": "Skeleton Barrel",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/skeleton_barrel.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Fisherman",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/fisherman.webp",
+        "rarity": "legendary"
+    },
+    {
+        "name": "Firecracker",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/firecracker.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Elixir Golem",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/elixir_golem.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Little Prince",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/little_prince.webp",
+        "rarity": "champion"
+    },
+    {
+        "name": "Goblin Machine",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/goblin_machine.webp",
+        "rarity": "legendary"
+    },
+    {
+        "name": "Valkyrie",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/valkyrie.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Musketeer",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/musketeer.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Baby Dragon",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/baby_dragon.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Mini P.E.K.K.A",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/mini_pekka.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Hog Rider",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/hog_rider.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Dark Prince",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/dark_prince.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Lumberjack",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/lumberjack.webp",
+        "rarity": "legendary"
+    },
+    {
+        "name": "Battle Ram",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/battle_ram.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Inferno Dragon",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/inferno_dragon.webp",
+        "rarity": "legendary"
+    },
+    {
+        "name": "Electro Wizard",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/electro_wizard.webp",
+        "rarity": "legendary"
+    },
+    {
+        "name": "Hunter",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/hunter.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Night Witch",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/night_witch.webp",
+        "rarity": "legendary"
+    },
+    {
+        "name": "Zappies",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/zappies.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Flying Machine",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/flying_machine.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Magic Archer",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/magic_archer.webp",
+        "rarity": "legendary"
+    },
+    {
+        "name": "Mighty Miner",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/mighty_miner.webp",
+        "rarity": "champion"
+    },
+    {
+        "name": "Battle Healer",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/battle_healer.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Skeleton King",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/skeleton_king.webp",
+        "rarity": "champion"
+    },
+    {
+        "name": "Golden Knight",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/golden_knight.webp",
+        "rarity": "champion"
+    },
+    {
+        "name": "Skeleton Dragons",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/skeleton_dragons.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Mother Witch",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/mother_witch.webp",
+        "rarity": "legendary"
+    },
+    {
+        "name": "Phoenix",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/pheonix.webp",
+        "rarity": "legendary"
+    },
+    {
+        "name": "Goblin Demolisher",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/goblin_demolisher.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Rune Giant",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/rune_giant.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Giant",
+        "cost": "5",
+        "icon": "/clash-royale/res/img/cards/giant.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Balloon",
+        "cost": "5",
+        "icon": "/clash-royale/res/img/cards/balloon.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Witch",
+        "cost": "5",
+        "icon": "/clash-royale/res/img/cards/witch.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Barbarians",
+        "cost": "5",
+        "icon": "/clash-royale/res/img/cards/barbarians.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Prince",
+        "cost": "5",
+        "icon": "/clash-royale/res/img/cards/prince.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Wizard",
+        "cost": "5",
+        "icon": "/clash-royale/res/img/cards/wizard.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Minion Horde",
+        "cost": "5",
+        "icon": "/clash-royale/res/img/cards/minion_horde.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Bowler",
+        "cost": "5",
+        "icon": "/clash-royale/res/img/cards/bowler.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Executioner",
+        "cost": "5",
+        "icon": "/clash-royale/res/img/cards/executioner.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Ram Rider",
+        "cost": "5",
+        "icon": "/clash-royale/res/img/cards/ram_rider.webp",
+        "rarity": "legendary"
+    },
+    {
+        "name": "Rascals",
+        "cost": "5",
+        "icon": "/clash-royale/res/img/cards/rascals.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Cannon Cart",
+        "cost": "5",
+        "icon": "/clash-royale/res/img/cards/cannon_cart.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Royal Hogs",
+        "cost": "5",
+        "icon": "/clash-royale/res/img/cards/royal_hogs.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Electro Dragon",
+        "cost": "5",
+        "icon": "/clash-royale/res/img/cards/electro_dragon.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Archer Queen",
+        "cost": "5",
+        "icon": "/clash-royale/res/img/cards/archer_queen.webp",
+        "rarity": "champion"
+    },
+    {
+        "name": "Monk",
+        "cost": "5",
+        "icon": "/clash-royale/res/img/cards/monk.webp",
+        "rarity": "champion"
+    },
+    {
+        "name": "Goblinstein",
+        "cost": "5",
+        "icon": "/clash-royale/res/img/cards/goblinstein.webp",
+        "rarity": "champion"
+    },
+    {
+        "name": "Giant Skeleton",
+        "cost": "6",
+        "icon": "/clash-royale/res/img/cards/giant_skeleton.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Royal Giant",
+        "cost": "6",
+        "icon": "/clash-royale/res/img/cards/royal_giant.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Sparky",
+        "cost": "6",
+        "icon": "/clash-royale/res/img/cards/sparky.webp",
+        "rarity": "legendary"
+    },
+    {
+        "name": "Elite Barbarians",
+        "cost": "6",
+        "icon": "/clash-royale/res/img/cards/elite_barbarians.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Goblin Giant",
+        "cost": "6",
+        "icon": "/clash-royale/res/img/cards/goblin_giant.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Boss Bandit",
+        "cost": "6",
+        "icon": "/clash-royale/res/img/cards/boss_bandit.webp",
+        "rarity": "champion"
+    },
+    {
+        "name": "Spirit Empress",
+        "cost": "6",
+        "icon": "/clash-royale/res/img/cards/spirit_empress.webp",
+        "rarity": "legendary"
+    },
+    {
+        "name": "P.E.K.K.A",
+        "cost": "7",
+        "icon": "/clash-royale/res/img/cards/pekka.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Lava Hound",
+        "cost": "7",
+        "icon": "/clash-royale/res/img/cards/lava_hound.webp",
+        "rarity": "legendary"
+    },
+    {
+        "name": "Royal Recruits",
+        "cost": "7",
+        "icon": "/clash-royale/res/img/cards/royal_recruits.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Mega Knight",
+        "cost": "7",
+        "icon": "/clash-royale/res/img/cards/mega_knight.webp",
+        "rarity": "legendary"
+    },
+    {
+        "name": "Electro Giant",
+        "cost": "7",
+        "icon": "/clash-royale/res/img/cards/electro_giant.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Golem",
+        "cost": "8",
+        "icon": "/clash-royale/res/img/cards/golem.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Three Musketeers",
+        "cost": "9",
+        "icon": "/clash-royale/res/img/cards/three_musketeers.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Cannon",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/cannon.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Tombstone",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/tombstone.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Mortar",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/mortar.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Bomb Tower",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/bomb_tower.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Tesla",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/tesla.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Furnace",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/furnace.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Goblin Cage",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/goblin_cage.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Goblin Drill",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/goblin_drill.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Goblin Hut",
+        "cost": "5",
+        "icon": "/clash-royale/res/img/cards/goblin_hut.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Inferno Tower",
+        "cost": "5",
+        "icon": "/clash-royale/res/img/cards/inferno_tower.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Elixir Collector",
+        "cost": "6",
+        "icon": "/clash-royale/res/img/cards/elixir_collector.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "X-Bow",
+        "cost": "6",
+        "icon": "/clash-royale/res/img/cards/x_bow.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Barbarian Hut",
+        "cost": "7",
+        "icon": "/clash-royale/res/img/cards/barbarian_hut.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Mirror",
+        "cost": "?",
+        "icon": "/clash-royale/res/img/cards/mirror.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Rage",
+        "cost": "2",
+        "icon": "/clash-royale/res/img/cards/rage.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Zap",
+        "cost": "2",
+        "icon": "/clash-royale/res/img/cards/zap.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "The Log",
+        "cost": "2",
+        "icon": "/clash-royale/res/img/cards/the_log.webp",
+        "rarity": "legendary"
+    },
+    {
+        "name": "Barbarian Barrel",
+        "cost": "2",
+        "icon": "/clash-royale/res/img/cards/barbarian_barrel.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Giant Snowball",
+        "cost": "2",
+        "icon": "/clash-royale/res/img/cards/giant_snowball.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Goblin Curse",
+        "cost": "2",
+        "icon": "/clash-royale/res/img/cards/goblin_curse.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Arrows",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/arrows.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Goblin Barrel",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/goblin_barrel.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Tornado",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/tornado.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Clone",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/clone.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Earthquake",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/earthquake.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Royal Delivery",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/royal_delivery.webp",
+        "rarity": "common"
+    },
+    {
+        "name": "Void",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/void.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Vines",
+        "cost": "3",
+        "icon": "/clash-royale/res/img/cards/vines.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Fireball",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/fireball.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Freeze",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/freeze.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Poison",
+        "cost": "4",
+        "icon": "/clash-royale/res/img/cards/poison.webp",
+        "rarity": "epic"
+    },
+    {
+        "name": "Graveyard",
+        "cost": "5",
+        "icon": "/clash-royale/res/img/cards/graveyard.webp",
+        "rarity": "legendary"
+    },
+    {
+        "name": "Rocket",
+        "cost": "6",
+        "icon": "/clash-royale/res/img/cards/rocket.webp",
+        "rarity": "rare"
+    },
+    {
+        "name": "Lightning",
+        "cost": "6",
+        "icon": "/clash-royale/res/img/cards/lightning.webp",
+        "rarity": "epic"
+    }
+]
